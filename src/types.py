c126321039636e09dcda77688580cbfd254ea55a
DalilaManu <dmoliveira1205@gmail.com>
2026-03-04 12:45:08 -0300

    refactor(types.py): :recycle: Refactor toString, inventory, nextDay

--- src/types.py
+++ src/types.py
@@ -10,4 +10,17 @@ class Ollivander:
     def addItem(self, item):
         self.items.append(item)
 
-        
+    def toString(self):
+        representation = ""
+        for item in self.items:
+            representation += item.toString()
+        return representation
+    
+    def inventory(self):
+        return self.items
+    
+    def nextDay(self):
+        for item in self.items:
+            item.updateQuality()
+            print(item.toString())   
+
